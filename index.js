20dc394c50dc59a39d99156feb4727928b7cc175
Pierre-Eric Pelloux-Prayer <pierre-eric@oslandia.com>
2018-02-09 14:42:39 +0100

    Improve script

--- index.js
+++ index.js
@@ -3,63 +3,81 @@ const fs = require('fs');
 const createCollage = require("photo-collage");
 
 // Parse example/index.html
-const path = process.argv[2] + '/examples/index.html';
-const data = fs.readFileSync(path, 'utf8');
+function extractExamplesFromIndex() {
+  const path = process.argv[2] + '/examples/index.html';
+  const data = fs.readFileSync(path, 'utf8');
+  return data.split("href=\'./").map((u) => {
+    const end = u.indexOf("'", 0);
+    return u.slice(0, end);
+  }).slice(1);
+}
 
-const examples = data.split("href=\'./").map((u) => {
-  const end = u.indexOf("'", 0);
-  return u.slice(0, end);
-});
+const examples = process.argv.length > 3 ?
+  process.argv.slice(3).map(n => `${n}.html`) :
+  extractExamplesFromIndex();
+
+console.log('URLs:', examples);
 
 const examplesImage = [];
-
+const start = Date.now();
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
-  for (const example of examples.slice(1)) {
+  for (const example of examples) {
     const page = await browser.newPage();
     page.setViewport({ width: 400, height: 300 });
-    process.stdout.write(`Loading ${example} `);
+    const exampleStart = Date.now();
+    process.stdout.write(`${(exampleStart - start) / 1000} s - loading ${example} `);
     await page.goto(`http://localhost:8080/examples/${example}`);
     process.stdout.write('.')
+    // page.on('console', msg => {
+    //   console.log((Date.now() - start) / 1000, ...msg.args().map(o => o.toString()));
+    // });
+    process.stdout.write('.')
     // Get the "viewport" of the page, as reported by the page.
-    const res = await page.evaluate(() => {
+    const res = await page.evaluate((example) => {
       function resolveOnLoad(x) {
         return new Promise(resolve => {
-          function getView(promise) {
+          function getView() {
+            console.log('getview...');
             if (typeof(view) === 'object') {
-              return promise ? promise.resolve(view) : Promise.resolve(view);
+              return onViewFound(view);
             }
             if (typeof(globeView) === 'object') {
-              return promise ? promise.resolve(globeView) : Promise.resolve(globeView);
+              return onViewFound(globeView);
             }
 
-            const p = new Promise((rrr) => {
-              this.resolve = rrr;
-            });
-            setTimeout(() => { getView(p) }, 100);
-            return p;
+            // console.warn(`FAIL to retrieve the View in ${example}`);
+            setTimeout(() => { getView() }, 100);
           }
+
+          getView();
+
           // setup default timeout
-          setTimeout(() => { resolve(false); }, 120000);
-          getView().then((v) => {
+          setTimeout(() => { resolve(false); }, 30000);
+
+          function onViewFound(v) {
             v.mainLoop.addEventListener('command-queue-empty', () => {
-              resolve(true);
-          })});
+              v.addFrameRequester('after_render', () => {
+                resolve(true);
+              });
+            });
+            v.notifyChange(false);
+          }
         });
       }
 
       return resolveOnLoad();
-    });
+    }, example);
     process.stdout.write('.')
 
     if (!res) {
-      process.stdout.write('☠')      
+      process.stdout.write('☠')
     }
 
     await page.screenshot({path: example.replace('html', 'png')});
     examplesImage.push(example.replace('html', 'png'));
-    process.stdout.write(`. wrote ${example.replace('html', 'png')}\n`);
+    process.stdout.write(`. wrote ${example.replace('html', 'png')} [${(Date.now() - exampleStart) / 1000} s]\n`);
   }
 
   await browser.close();
@@ -72,7 +90,7 @@ const examplesImage = [];
     imageHeight: 300,
     spacing: 2,
   };
-   
+
   createCollage(options)
     .then((canvas) => {
       const src = canvas.jpegStream();
